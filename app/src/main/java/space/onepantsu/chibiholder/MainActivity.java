8e41c28ee759e7adf80f07e6ce7e248f751ff79a
Saimon <saimon22011@gmail.com>
2022-02-08 22:22:06 +0300

    LoginRegistr

--- app/src/main/java/space/onepantsu/chibiholder/MainActivity.java
+++ app/src/main/java/space/onepantsu/chibiholder/MainActivity.java
@@ -2,7 +2,13 @@ package space.onepantsu.chibiholder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import javax.xml.parsers.SAXParser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
